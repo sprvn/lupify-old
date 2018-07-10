6c88bd835cd222a7ae3f0aea4d7fab069f24b1bf
Kevin Gustafsson <kevin.gustafsson@clxcommunications.com>
2018-07-10 21:20:29 +0200

    Added option to change lupify ports and added an openldap container to development environment

--- configs/config_script.sh
+++ configs/config_script.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-sed -i s/80/${LUPIFY_HTTP_PORT}/g /etc/nginx/conf.d/uwsgi-nginx.conf 
-sed -i s/443/${LUPIFY_HTTPS_PORT}/g /etc/nginx/conf.d/uwsgi-nginx.conf
+sed -i s/HTTP_PORT_TMP/${LUPIFY_HTTP_PORT}/g /etc/nginx/conf.d/uwsgi-nginx.conf 
+sed -i s/HTTPS_PORT_TMP/${LUPIFY_HTTPS_PORT}/g /etc/nginx/conf.d/uwsgi-nginx.conf
 
 
 exec "$@"
